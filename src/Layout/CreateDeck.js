314cccda3631167493bdf1c82d38d74844f5e51a
Molly <mojonesycreative@gmail.com>
2022-05-02 18:23:57 -0400

    initial files commit

---
+++ src/Layout/CreateDeck.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CreateDeck(){
+
+};
+
+export default CreateDeck;
---
+++ src/Layout/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+// Components
+import Header from "./Header";
+import NotFound from "./NotFound";
+
+function Layout() {
+  return (
+    <>
+      <Header />
+      <div className="container">
+        {/* TODO: Implement the screen starting here */}
+        <NotFound />
+      </div>
+    </>
+  );
+}
+
+export default Layout;
