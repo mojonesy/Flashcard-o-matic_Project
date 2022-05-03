93a58ac0c5d39950948344079d240a6fb21a2068
Molly <mojonesycreative@gmail.com>
2022-05-03 15:41:44 -0400

    changed index.js to Home.js in Home directory

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import Home from "../Home";
+import Home from "../Home/Home";
 import Header from "./Header";
 import NotFound from "./NotFound";
 
