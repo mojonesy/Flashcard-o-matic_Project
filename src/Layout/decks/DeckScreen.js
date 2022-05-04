d87fdc4035c4e2619197034611eb5651ca647dae
Molly <mojonesycreative@gmail.com>
2022-05-04 12:10:23 -0400

    created DeckScreen function

---
+++ src/Layout/decks/DeckScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import DeckList from "./DeckList";
+
+function DeckScreen() {
+    return (
+        <>
+        <DeckList />
+        </>
+    );
+};
+
+export default DeckScreen;
