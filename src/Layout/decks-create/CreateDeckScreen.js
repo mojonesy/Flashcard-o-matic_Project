1b12abc440d284404cbb601b58424999ad1af13a
Molly <mojonesycreative@gmail.com>
2022-05-04 12:06:46 -0400

    created CreateDeckScreen function

---
+++ src/Layout/decks-create/CreateDeckScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CreateDeck from "./CreateDeck";
+
+function CreateDeckScreen() {
+    return (
+        <>
+        <CreateDeck />
+        </>
+    );
+}
+
+export default CreateDeckScreen;
