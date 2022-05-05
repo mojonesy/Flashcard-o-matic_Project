e8340741433666f70a74b963fc529d3b78f30365
Molly <mojonesycreative@gmail.com>
2022-05-05 17:54:30 -0400

    renamed deletebutton to deletedeckbutton & implemented window.confirm()

---
+++ src/Layout/Home/DeleteDeckButton.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { deleteDeck } from "../../utils/api";
+
+function DeleteDeckButton({ deckId }) {
+    const handleDelete = () => {
+        if (window.confirm("Delete this deck?\n\nYou will not be able to recover it.")) {
+            deleteDeck(deckId);
+        }
+    };
+
+    return (
+        <button onClick={handleDelete} className="btn btn-danger">
+            <span className="oi oi-trash" />
+        </button>
+    );
+}
+
+export default DeleteDeckButton;
