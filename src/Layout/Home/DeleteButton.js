e60dbae69f78bad5c025cf151d0607c7f318ad04
Molly <mojonesycreative@gmail.com>
2022-05-04 21:31:19 -0400

    return button

---
+++ src/Layout/Home/DeleteButton.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function DeleteButton() {
+    // TODO: implement window.confirm //
+
+    return (
+        <button className="btn btn-danger">
+            <span className="oi oi-trash" />
+        </button>
+    )
+}
+
+export default DeleteButton;
