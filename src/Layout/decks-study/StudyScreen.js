0cc88ec36a2a5672b6b6b59dbd4ff2fc4dee82fc
Molly <mojonesycreative@gmail.com>
2022-05-06 11:18:19 -0400

    load deck and cards, added nav bar & NotEnoughCards

---
+++ src/Layout/decks-study/StudyScreen.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import { readDeck } from "../../utils/api";
+import { useParams, Link } from "react-router-dom";
+// Components //
+import FlipButton from "./FlipButton";
+import NotEnoughCards from "./NotEnoughCards";
+
+function StudyScreen() {
+    const deckId = useParams().deckId;
+    const [deck, setDeck] = useState([]);
+    const [cards, setCards] = useState([]);
+
+    // Load deck using deckId //
+    useEffect(() => {
+        async function loadDeck() {
+            const response = readDeck(deckId);
+            const apiResponse = await response;
+            setDeck(apiResponse);
+            setCards(apiResponse.cards);
+        }
+        loadDeck();
+    }, [deckId]);
+
+
+    return (
+        <>
+        <nav aria-label="breadcrumb">
+            <ol className="breadcrumb">
+                <li className="breadcrumb-item">
+                    <Link to="/" style={{color:"teal"}}>
+                    <span className="oi oi-home" style={{marginRight: "5px"}} />
+                    Home
+                    </Link>
+                </li>
+                <li className="breadcrumb-item" style={{color:"teal"}}>{deck.name}</li>
+                <li className="breadcrumb-item active" aria-current="page">Study</li>
+            </ol>
+        </nav>
+
+        <h1>{deck.name}: Study</h1>
+        
+
+            <div id="card">
+                {/* Display front of card. When Flip button is clicked, display back of card.
+                    If Flip button is clicked again, display front of card. If Next button is
+                    clicked, display next card in deck. */}
+                
+            </div>
+        <NotEnoughCards cardsNumber={cards.length} />
+        </>
+    );
+}
+
+export default StudyScreen;
