43666f2866c36d6b6f963e14c9cbe8bc4259a29c
Molly <mojonesycreative@gmail.com>
2022-05-06 11:17:26 -0400

    Added Flip button for study screen

---
+++ src/Layout/decks-study/FlipButton.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function FlipButton() {
+    return (
+        <>
+        <button className="btn btn-secondary" >Flip</button>
+        </>
+    );
+}
+
+export default FlipButton;
