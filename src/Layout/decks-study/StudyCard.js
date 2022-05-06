d5ebf4882a88e3d213c1198f0e36d37278e6955a
Molly <mojonesycreative@gmail.com>
2022-05-06 12:43:10 -0400

    implement if stmnt to set front/back view of card

--- src/Layout/decks-study/StudyCard.js
+++ src/Layout/decks-study/StudyCard.js
@@ -3,16 +3,44 @@ import { useState } from "react";
 
 function StudyCard({ currentCard, setCurrentCard, cards }) {
     const [cardNum, setCardNum] = useState(1);
-    
+    const [frontOfCard, setFrontOfCard] = useState(true);
 
+    const handleFlip = () => {
+        if (frontOfCard) {
+            setFrontOfCard(false);
+        } else {
+            setFrontOfCard(true);
+        }
+    }
+
+
+    if (frontOfCard) {
+        return (
+            <>
+            <div className="card">
+            <div className="card-body">
+                <h5 className="card-title">Card {cardNum} of {cards.length}</h5>
+                <p className="card-text">{currentCard.front}</p>
+                <button 
+                    className="btn btn-secondary"
+                    onClick={handleFlip}
+                    >
+                    Flip
+                </button>
+            </div>
+            </div>
+            </>
+        );
+    }
     return (
         <>
         <div className="card">
          <div className="card-body">
             <h5 className="card-title">Card {cardNum} of {cards.length}</h5>
-            <p className="card-text">{currentCard.front}</p>
+            <p className="card-text">{currentCard.back}</p>
             <button 
                 className="btn btn-secondary"
+                onClick={handleFlip}
                 >
                 Flip
             </button>
