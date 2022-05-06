21ae19a7c76472a25362ce0e8657b82b9642013f
Molly <mojonesycreative@gmail.com>
2022-05-06 13:27:50 -0400

    handle next- window.confirm() to restart or go home

--- src/Layout/decks-study/StudyCard.js
+++ src/Layout/decks-study/StudyCard.js
@@ -1,15 +1,34 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import { useState } from "react";
 
 function StudyCard({ currentCard, setCurrentCard, cards }) {
+    const history = useHistory();
     const [cardNum, setCardNum] = useState(1);
     const [frontOfCard, setFrontOfCard] = useState(true);
 
+    // Handle flip //
     const handleFlip = () => {
         if (frontOfCard) {
             setFrontOfCard(false);
         } else {
             setFrontOfCard(true);
+        }
+    }
+    // Handle next //
+    const handleNext = () => {
+        if (cardNum < cards.length) {
+            setCurrentCard(cards[cardNum]);
+            setFrontOfCard(true);
+            setCardNum(cardNum + 1);
+        } else {
+            if (window.confirm("Restart Cards?\n\nClick 'cancel' to return to the home page.")){
+                setCurrentCard(cards[0]);
+                setFrontOfCard(true);
+                setCardNum(1);
+            } else {
+                history.push("/");
+            }
         }
     }
 
@@ -40,9 +59,17 @@ function StudyCard({ currentCard, setCurrentCard, cards }) {
             <p className="card-text">{currentCard.back}</p>
             <button 
                 className="btn btn-secondary"
+                style={{marginRight:"10px"}}
                 onClick={handleFlip}
                 >
                 Flip
+            </button>
+            <button
+                className="btn btn-success"
+                style={{backgroundColor:"teal"}}
+                onClick={handleNext}
+                >
+                Next
             </button>
          </div>
         </div>
