8aa9e94a694a1fabe6437e8fc3f049eb36353655
Molly <mojonesycreative@gmail.com>
2022-05-07 12:12:37 -0400

    renamed to CreateDeckScreen

--- src/Layout/decks-create/CreateDeckScreen.js
+++ src/Layout/decks-create/CreateDeckScreen.js
@@ -1,12 +1,97 @@
 import React from "react";
-import CreateDeck from "./CreateDeck";
+import { createDeck } from "../../utils/api/index";
+import { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
 
-function CreateDeckScreen() {
+function CreateDeckScreen(){
+    const history = useHistory();
+    
+    // Set initial empty form state //
+    const initialFormState = {
+        name: "",
+        description: ""
+    };
+    const [deckData, setDeckData] = useState({ ...initialFormState });
+    
+    
+    // Input change handler //
+    const handleChange = ({ target }) => {
+        setDeckData({ ...deckData, [target.name]: target.value});
+    };
+
+    // Handle submit, createDeck(), return to home page //
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        async function loadCreatedDeck() {
+            const response = await createDeck(deckData);
+            console.log(response);
+        }
+        loadCreatedDeck();
+        setDeckData({ ...initialFormState });
+        history.push("/");
+    };
+
+
     return (
         <>
-        <CreateDeck />
+            <nav aria-label="breadcrumb">
+                <ol className="breadcrumb">
+                    <li className="breadcrumb-item">
+                        <Link to="/" style={{color:"teal"}}>
+                        <span className="oi oi-home" style={{marginRight: "5px"}} />
+                        Home
+                        </Link>
+                    </li>
+                    <li className="breadcrumb-item active" aria-current="page">Create Deck</li>
+                </ol>
+            </nav>
+
+            <h1>Create Deck</h1>
+
+            <form onSubmit={handleSubmit}>
+                <div className="mb-3">
+                    <label htmlFor="name" className="form-label">Name</label>
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        id="name"
+                        name="name" 
+                        placeholder="Deck Name"
+                        required={true}
+                        onChange={handleChange}
+                        value={deckData.name} 
+                    />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="description" className="form-label">Description</label>
+                    <textarea 
+                        className="form-control" 
+                        id="description" 
+                        name="description"
+                        placeholder="Brief description of the deck"
+                        required={true} 
+                        style={{height: "150px"}}
+                        onChange={handleChange}
+                        value={deckData.description}
+                    />
+                </div>
+                <button 
+                    type="button" 
+                    className="btn btn-secondary btn-lg"
+                    style={{marginRight: "10px", marginBottom: "20px"}}
+                    onClick={() => history.push("/")}
+                    > Cancel
+                </button>
+                <button 
+                    type="Submit" 
+                    className="btn btn-primary btn-lg"
+                    style={{marginBottom: "20px"}}
+                    > Submit
+                </button>
+            </form>
         </>
     );
-}
+};
 
 export default CreateDeckScreen;
