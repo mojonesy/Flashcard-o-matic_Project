564e186f5e9e619d495bbec3a337746bf3aceb8d
Molly <mojonesycreative@gmail.com>
2022-05-08 11:44:14 -0400

    implement cardform component

--- src/Layout/deck-cards/EditCardScreen.js
+++ src/Layout/deck-cards/EditCardScreen.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { useParams, useHistory, Link } from "react-router-dom";
 import { readDeck, readCard, updateCard } from "../../utils/api/index";
+import CardForm from "./CardForm";
 
 function EditCardScreen() {
     const history = useHistory();
@@ -58,47 +59,18 @@ function EditCardScreen() {
         </nav>
 
         <h1>Edit Card</h1>
-            <form onSubmit={handleSubmit}>
-                <div className="mb-3">
-                    <label htmlFor="front" className="form-label">Front</label>
-                    <textarea 
-                        className="form-control" 
-                        id="front"
-                        name="front" 
-                        placeholder={card.front}
-                        required={true}
-                        style={{height: "150px"}}
-                        onChange={handleChange}
-                        value={card.front} 
-                    />
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="back" className="form-label">Back</label>
-                    <textarea 
-                        className="form-control" 
-                        id="back" 
-                        name="back"
-                        placeholder={card.back}
-                        required={true} 
-                        style={{height: "150px"}}
-                        onChange={handleChange}
-                        value={card.back}
-                    />
-                </div>
-                <button 
-                    type="button" 
-                    className="btn btn-secondary btn-lg"
-                    style={{marginRight: "10px", marginBottom: "20px"}}
-                    onClick={() => history.push(`/decks/${deckId}`)}
-                    > Cancel
-                </button>
-                <button 
-                    type="Submit" 
-                    className="btn btn-primary btn-lg"
-                    style={{marginBottom: "20px"}}
-                    > Save
-                </button>
-            </form>
+
+            <CardForm 
+                handleSubmit={handleSubmit}
+                handleChange={handleChange}
+                deckId={deckId}
+                placeholderFront={card.front}
+                placeholderBack={card.back}
+                valueFront={card.front}
+                valueBack={card.back}
+                secondaryButton="Cancel"
+                primaryButton="Save"
+            />
         </>
     );
 }
